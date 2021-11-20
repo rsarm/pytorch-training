49ba0c0666d3546549010a203c31f44882e9147e
rafael <rafael.sarmiento@cscs.ch>
2021-11-21 00:50:27 +0100

    review t5

--- t5_xsum/t5-text-summarization-deepspeed.py
+++ t5_xsum/t5-text-summarization-deepspeed.py
@@ -31,29 +31,31 @@ model = AutoModelForSeq2SeqLM.from_pretrained(
 raw_datasets = load_dataset('xsum')
 
 max_source_length = 128  # 1024
-max_target_length = 128
+max_target_length = 64
 ignore_pad_token_for_loss = True
 padding = False            # else 'max_length'
 label_pad_token_id = -100  # else tokenizer.pad_token_id
 per_device_train_batch_size = 4
 per_device_eval_batch_size = 4
 
+
 def preprocess_function(examples, text_column='document',
                         summary_column='summary', prefix='summarize: '):
     inputs = examples[text_column]
     targets = examples[summary_column]
     inputs = [prefix + inp for inp in inputs]
-    model_inputs = tokenizer(inputs, max_length=max_source_length, padding=padding, truncation=True)
+    model_inputs = tokenizer(inputs, max_length=max_source_length,
+                             padding=padding, truncation=True)
 
     # Setup the tokenizer for targets
     with tokenizer.as_target_tokenizer():
-        labels = tokenizer(targets, max_length=max_target_length, padding=padding, truncation=True)
+        labels = tokenizer(targets, max_length=max_target_length,
+                           padding=padding, truncation=True)
 
-    # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
-    # padding in the loss.
     if padding == "max_length" and ignore_pad_token_for_loss:
         labels["input_ids"] = [
-            [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in labels["input_ids"]
+            [(l if l != tokenizer.pad_token_id else -100) for l in label]
+            for label in labels["input_ids"]
         ]
 
     model_inputs["labels"] = labels["input_ids"]
@@ -93,11 +95,13 @@ weight_decay = 0.0
 no_decay = ["bias", "LayerNorm.weight"]
 optimizer_grouped_parameters = [
     {
-        "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+        "params": [p for n, p in model.named_parameters()
+                   if not any(nd in n for nd in no_decay)],
         "weight_decay": weight_decay,
     },
     {
-        "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
+        "params": [p for n, p in model.named_parameters()
+                   if any(nd in n for nd in no_decay)],
         "weight_decay": 0.0,
     },
 ]
@@ -109,6 +113,7 @@ model_engine, optimizer, trainloader, __ = deepspeed.initialize(
     training_data=train_dataset, collate_fn=data_collator
 )
 
+
 def print_peak_memory(prefix, device):
     if device == 0:
         print(f"{prefix}: {torch.cuda.max_memory_allocated(device) // 1e6}MB ")
@@ -119,11 +124,11 @@ print_peak_memory("Max memory allocated after creating DDP", 0)
 # model.train();
 for epoch in range(1):
     for step, batch in enumerate(train_dataloader):
-        if step > 100:
-           break
-
         optimizer.zero_grad()
         outputs = model(**batch.to(model_engine.device))
         loss = outputs.loss
         model_engine.backward(loss)
         model_engine.step()
+
+        if step > 100:
+            break
